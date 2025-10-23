fc163f84c3e012feddad85068b7f554489467673
Nizar <79638878+zaddemnizar@users.noreply.github.com>
2025-10-23 20:49:34 +0100

    modification vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+
+  base: "/zaddem-insight-hub/",
+  
   server: {
     host: "::",
     port: 8080,
